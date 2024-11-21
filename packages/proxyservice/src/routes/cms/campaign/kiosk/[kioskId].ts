67408ee531a72645301e83716105322f46bcfeca
Puyodead1 <puyodead@proton.me>
2024-11-20 19:34:01 -0500

    proxyservice: disable cms campaigns

--- packages/proxyservice/src/routes/cms/campaign/kiosk/[kioskId].ts
+++ packages/proxyservice/src/routes/cms/campaign/kiosk/[kioskId].ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import { v4 } from "uuid";
 import { KioskCampaignsRequest } from "../../../../interfaces";
 import { KioskCampaignsRequestSchema } from "../../../../schemas";
-import { EAssetTarget, EControlType, EInCartType, IKioskCampaignsResponse } from "../../../../types";
+import { IKioskCampaignsResponse } from "../../../../types";
 
 export const post = [
     celebrate({
@@ -20,35 +20,36 @@ export const post = [
             Success: true,
             Errors: [],
             InCarts: [
-                {
-                    CampaignInCartId: 1,
-                    InCartType: EInCartType.StartScreen,
-                    PromoCode: "PROMO",
-                    ExcludeTitles: [],
-                },
+                // this gave a "1 free movie" promo popup
+                // {
+                //     CampaignInCartId: 1,
+                //     InCartType: EInCartType.StartScreen,
+                //     PromoCode: "PROMO",
+                //     ExcludeTitles: [],
+                // },
             ],
             StartScreens: [
-                {
-                    Controls: [
-                        {
-                            ControlType: EControlType.StartAsset,
-                            Asset: {
-                                Id: 1,
-                                Name: "Asset 1",
-                                Url: "https://picsum.photos/200",
-                                FullPath: "https://picsum.photos/200",
-                            },
-                            ControlId: 1,
-                            DisplayDuration: 1000,
-                            IncludeIfNoInventory: true,
-                            Order: 1,
-                            MaxTitles: 100,
-                            ShowPressToStart: true,
-                            Target: EAssetTarget.PromoCode,
-                            TargetValue: "1",
-                        },
-                    ],
-                },
+                // {
+                //     Controls: [
+                //         {
+                //             ControlType: EControlType.StartAsset,
+                //             Asset: {
+                //                 Id: 1,
+                //                 Name: "Asset 1",
+                //                 Url: "https://picsum.photos/200",
+                //                 FullPath: "https://picsum.photos/200",
+                //             },
+                //             ControlId: 1,
+                //             DisplayDuration: 1000,
+                //             IncludeIfNoInventory: true,
+                //             Order: 1,
+                //             MaxTitles: 100,
+                //             ShowPressToStart: true,
+                //             Target: EAssetTarget.PromoCode,
+                //             TargetValue: "1",
+                //         },
+                //     ],
+                // },
             ],
         } as IKioskCampaignsResponse);
     },
