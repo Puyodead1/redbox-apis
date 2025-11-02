5ba8a0c5b840566df8eef092d193bfe4d654eb1f
Puyodead1 <puyodead@proton.me>
2025-11-01 23:57:08 -0400

    add all aedes events for better logging and debugging

--- packages/mqtt-broker/src/index.ts
+++ packages/mqtt-broker/src/index.ts
@@ -35,8 +35,65 @@ const HOST = config.mqttConfig.host;
     logger.info(`Server is listening on ${HOST}:${PORT}`);
   });
 
+  aedes.on("client", (client) => {
+    logger.info(`Client connected: ${client.id}`);
+  });
+
+  aedes.on("clientDisconnect", (client) => {
+    logger.info(`Client disconnected: ${client.id}`);
+  });
+
+  aedes.on("clientError", (client, err) => {
+    logger.error(`Client error on ${client.id}: ${err.message}`);
+  });
+
+  aedes.on("clientReady", (client) => {
+    logger.info(`Client ready: ${client.id}`);
+  });
+
+  aedes.on("subscribe", (subscriptions, client) => {
+    const topics = subscriptions.map((sub) => sub.topic).join(", ");
+    logger.info(`Client "${client.id}" subscribed to topics: ${topics}`);
+  });
+
+  aedes.on("unsubscribe", (subscriptions, client) => {
+    const topics = subscriptions.join(", ");
+    logger.info(`Client "${client.id}" unsubscribed from topics: ${topics}`);
+  });
+
+  aedes.on("ping", (packet, client) => {
+    logger.info(`Ping received from client "${client.id}"`);
+  });
+
+  aedes.on("ack", (packet, client) => {
+    logger.info(
+      `ACK received from client "${client.id}" for packet ID: ${packet.messageId}`,
+    );
+  });
+
+  aedes.on("closed", () => {
+    logger.info("Aedes broker closed");
+  });
+
+  aedes.on("connackSent", (packet, client) => {
+    logger.info(`CONNACK sent to client "${client.id}"`);
+  });
+
+  aedes.on("connectionError", (client, err) => {
+    logger.error(
+      `Connection error on client "${client ? client.id : "unknown"}": ${
+        err.message
+      }`,
+    );
+  });
+
+  aedes.on("keepaliveTimeout", (client) => {
+    logger.warn(`Keepalive timeout for client "${client.id}"`);
+  });
+
   aedes.on("publish", async (packet, client) => {
     if (!client) return; // ignore broker
+    console.log(packet);
 
     const topic = packet.topic;
     const message = packet.payload.toString();
@@ -44,5 +101,7 @@ const HOST = config.mqttConfig.host;
     logger.info(
       `Message received on topic "${topic}" from client "${client.id}": ${message}`,
     );
+
+    // TODO: publish response
   });
 })();
