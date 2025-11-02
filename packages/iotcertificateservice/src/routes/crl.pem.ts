77c6a823991ae0072c1c909f1e14568abd07e3ce
Puyodead1 <puyodead@proton.me>
2025-11-01 23:01:33 -0400

    Update crl.pem.ts

--- packages/iotcertificateservice/src/routes/crl.pem.ts
+++ packages/iotcertificateservice/src/routes/crl.pem.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import fs from "fs";
 import { getPathRelativeRoot } from "@redbox-apis/common";
 
+// a CRL endpoint, tbh i dont even know if this is needed?
+// mqtt client was complaining about certification revocation checking, but i dont see it actually calling the endpoint
+// TODO: if we do keep this, maybe we should implement other ways of revoking certs rather than just expiry date? idfk
 export const get = async (req: Request, res: Response) => {
   if (req.method !== "GET") return res.status(405);
 
